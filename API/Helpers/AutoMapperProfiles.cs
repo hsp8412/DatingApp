d1bea1a8692fd81a710c8142e3964773a2cdcb66
Sipeng He <hsp8412@hotmail.com>
2023-12-30 15:10:08 -0700

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>()
+        .ForMember(dest => dest.PhotoUrl, opt=>opt.MapFrom(src => src.Photos.Where(x=>x.IsMain).Select(x=>x.Url).FirstOrDefault()))
+        .ForMember(dest => dest.Age, opt=> opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+        CreateMap<Photo, PhotoDto>();
+    }
+}
